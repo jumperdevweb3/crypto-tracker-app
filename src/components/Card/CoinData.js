f2ab88fac4c8caa4a88bde87eebcc7c0ae0e0b6a
skoczy01 <skoczy01@wp.pl>
2022-07-23 00:09:11 +0200

    Fetch test data and use it

--- src/components/Card/CoinData.js
+++ src/components/Card/CoinData.js
@@ -2,44 +2,54 @@ import { Fragment } from "react";
 import { useWindowSize } from "../../Hooks/use-windowSize";
 import classes from "./CoinData.module.scss";
 
-export const CoinData = () => {
+export const CoinData = (props) => {
   const { width } = useWindowSize();
+  const {
+    image,
+    name,
+    symbol,
+    current_price,
+    market_cap_rank,
+    price_change_1h,
+    price_change_24h,
+    price_change_7d,
+    market_cap,
+    total_volume,
+  } = props;
+  const interNumberFormat = new Intl.NumberFormat("en-US");
   return (
     <div className={classes.coin}>
       <div className={classes.rank}>
-        <p>1</p>
+        <p>{market_cap_rank}</p>
       </div>
       <div className={classes.name}>
-        <img
-          src="https://s2.coinmarketcap.com/static/img/coins/64x64/1.png"
-          alt="asd"
-        />
-        <p>Bitcoin</p>
-        <span>BTC</span>
+        <img src={image} alt="" />
+        <p>{name}</p>
+        <span>{symbol.toUpperCase()}</span>
       </div>
       <div className={classes.price}>
-        <p>23,123,22</p>
+        <p>${interNumberFormat.format(current_price)}</p>
       </div>
       <div className={classes.time}>
-        <p>12 %</p>
+        <p>{price_change_1h.toFixed(2)}%</p>
       </div>
       {width >= 768 && (
         <Fragment>
           <div className={classes.time}>
-            <p>24 %</p>
+            <p>{price_change_24h.toFixed(2)}%</p>
           </div>
           <div className={classes.time}>
-            <p>73 %</p>
+            <p>{price_change_7d.toFixed(2)}%</p>
           </div>
         </Fragment>
       )}
       {width >= 1024 && (
         <Fragment>
           <div className={classes["market-cup"]}>
-            <p>$440,653,780,866</p>
+            <p>${interNumberFormat.format(market_cap)}</p>
           </div>
           <div className={classes.volume}>
-            <p>$30,752,532,795</p>
+            <p>${interNumberFormat.format(total_volume)}</p>
           </div>
         </Fragment>
       )}
--- src/components/CurrenciesList.js
+++ src/components/CurrenciesList.js
@@ -1,12 +1,61 @@
-import { Fragment } from "react";
+import { useEffect, useState } from "react";
+import { useFetchData } from "../Hooks/use-fetchData";
 import { CurrenciesOptions } from "./CurrenciesOptions";
 import { CoinData } from "./Card/CoinData";
+import { LoadingSpinner } from "./Ui/LoadingSpinner";
 
 export const CurrenciesList = () => {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const data = useFetchData(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=1&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d"
+  );
+  useEffect(() => {
+    const getData = async () => {
+      await data;
+      data.then(function (value) {
+        setFetchedData(value);
+        console.log(value);
+      });
+    };
+    getData();
+  }, []);
+
+  const items = fetchedData.map((item) => {
+    const {
+      image,
+      name,
+      symbol,
+      current_price,
+      market_cap_rank,
+      price_change_percentage_1h_in_currency,
+      price_change_percentage_24h_in_currency,
+      price_change_percentage_7d_in_currency,
+      market_cap,
+      total_volume,
+    } = item;
+
+    return (
+      <CoinData
+        key={item.id}
+        image={image}
+        name={name}
+        symbol={symbol}
+        current_price={current_price}
+        market_cap_rank={market_cap_rank}
+        price_change_1h={price_change_percentage_1h_in_currency}
+        price_change_24h={price_change_percentage_24h_in_currency}
+        price_change_7d={price_change_percentage_7d_in_currency}
+        market_cap={market_cap}
+        total_volume={total_volume}
+      />
+    );
+  });
   return (
     <div className="market-list">
       <CurrenciesOptions />
-      <CoinData />
+      {items}
+      <button>Fetch data </button>
     </div>
   );
 };
