4d38996a068dca77a267d72eb1e2522ecfb49c24
skoczy01 <skoczy01@wp.pl>
2022-07-26 11:57:27 +0200

    Fix and secure saving data

--- src/components/Pages/Watchlist.js
+++ src/components/Pages/Watchlist.js
@@ -3,11 +3,23 @@ import { CoinData } from "../Card/CoinData";
 
 export const Watchlist = () => {
   const data = useSelector((state) => state.watchlist.watchItems);
+  // const getData = localStorage.getItem("watchlist");
+  // const datas = JSON.parse(getData);
+  // console.log(data);
   return (
     <>
-      {data.map((item) => {
-        return <CoinData key={Math.random() * 100} {...item} />;
-      })}
+      {data.length !== 0 && (
+        <h2 className="center-item">Your's watch list items.</h2>
+      )}
+      {data.length !== 0 &&
+        data.map((item) => {
+          return <CoinData key={Math.random() * 100} {...item} />;
+        })}
+      {data.length === 0 && (
+        <h2 className="center-item">
+          No items, add some coin to your watchlist!
+        </h2>
+      )}
     </>
   );
 };
