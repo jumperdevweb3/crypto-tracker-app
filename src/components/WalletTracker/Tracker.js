5b2bf0c4bbac680e92d59e37f9eedfabe4f76c60
skoczy01 <skoczy01@wp.pl>
2022-07-31 17:06:10 +0200

    Add api browser

--- src/components/WalletTracker/Tracker.js
+++ src/components/WalletTracker/Tracker.js
@@ -1,25 +1,56 @@
 import classes from "./Tracker.module.scss";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { FaSearch } from "react-icons/fa";
+import { fetchEtherScanData } from "../../Store/currencies-actions";
+import { LoadingSpinner } from "../Ui/LoadingSpinner";
 
 export const Tracker = () => {
+  const fetchResult = useSelector((state) => state.scanner.result);
+  const fetchErrorMessage = useSelector((state) => state.scanner.errorMessage);
+  const isLoading = useSelector((state) => state.scanner.isLoading);
+  const [inputValue, setInputValue] = useState("");
+  const dispatch = useDispatch();
+
+  const inputChangeHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+  const fetchDataHandler = (event) => {
+    event.preventDefault();
+    dispatch(fetchEtherScanData(inputValue));
+    setInputValue("");
+  };
+
+  const result = fetchErrorMessage ? 0 : fetchResult * Math.pow(10, -18);
   return (
     <div className={classes.container}>
-      <div className={classes.box}>
+      <form className={classes.box} onSubmit={fetchDataHandler}>
         <input
           className={classes["input-search"]}
           type="search"
           placeholder="Search by wallet address to show ether balance..."
+          value={inputValue}
+          onChange={inputChangeHandler}
         />
-        <button>
+        <button type="submit">
           <FaSearch fontSize="1.5rem" color="rgb(193, 162, 222)" />
         </button>
-      </div>
+      </form>
+      {isLoading && <LoadingSpinner />}
       <div className={classes.result}>
         <p>
-          Entered Wallet have a <span className={classes.amount}>31,243 </span>
+          Entered Wallet have a{" "}
+          <span className={classes.amount}>
+            {result ? result.toFixed(10) : 0}{" "}
+          </span>
           ETH
         </p>
       </div>
+      <p className={classes.error}>{fetchErrorMessage && fetchErrorMessage}</p>
+      <p className={classes.description}>
+        *You can use test wallet address -
+        0x0D992fF8cd5c417Ce6c935A6d36e027f91119Ccf
+      </p>
     </div>
   );
 };
