d4c27b4b54b8a26efda5e506fc901a446c23163d
skoczy01 <skoczy01@wp.pl>
2022-08-01 13:17:09 +0200

    Add footer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,9 +3,12 @@ import classes from "./Layout.module.scss";
 
 export const Layout = ({ children }) => {
   return (
-    <>
+    <div className={classes.container}>
       <MainNav />
       <main className={classes.main}>{children}</main>
-    </>
+      <footer>
+        <a href="https://github.com/skoczy01">Check my GitHub</a>
+      </footer>
+    </div>
   );
 };
