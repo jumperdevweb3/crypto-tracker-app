fc7e934913f39c1eedbc4287c3a47ea924fce9fc
skoczy01 <skoczy01@wp.pl>
2022-08-24 20:31:32 +0200

    Refactor

--- src/components/types/types.ts
+++ src/components/types/types.ts
@@ -1,3 +1,5 @@
+import React, { ReactNode } from "react";
+
 export interface CurrencyItem {
   id: string;
   ath: number;
@@ -32,5 +34,5 @@ export interface CurrenciesState {
 
 export interface ModalTypes {
   onClose: () => void;
-  children: any;
+  children: ReactNode;
 }
