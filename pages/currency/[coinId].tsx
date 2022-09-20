85e21287b0797fa8384823f5f2bc0c0e8e426662
skoczy01 <filip.skoczylas00@wp.pl>
2022-09-20 21:20:28 +0200

    refactor: move to subpage folder

--- pages/currency/[coinId].tsx
+++ pages/currency/[coinId].tsx
@@ -1,14 +1,19 @@
-import { CurrenciesDetail } from "../components/currencies/CurrenciesDetail";
+import { CurrenciesDetail } from "../../components/currencies/CurrenciesDetail";
 import { useSelector } from "react-redux";
-import { RootState } from "../store/store";
+import { RootState } from "../../store/store";
 import { useRouter } from "next/router";
-import { CurrencyItem } from "../components/types/types";
+import { CurrencyItem } from "../../components/types/types";
+
 export default function CoinDetailPage() {
   const items = useSelector((state: RootState) => state.currencies.items);
 
+  type Path = string | string[] | undefined;
+
   const router = useRouter();
-  const idPath: string | string[] | undefined = router.query.coinId;
+  const idPath: Path = router.query.coinId;
   const item: CurrencyItem | any = items.find((item) => item.id === idPath);
+
+  if (!item) return <p>Not found</p>;
 
   return <>{items.length !== 0 && <CurrenciesDetail item={item} />}</>;
 }
