56b7a2141bcea40a93124cd77d0921a2cd44d04d
skoczy01 <filip.skoczylas00@wp.pl>
2022-09-20 21:19:42 +0200

    refactor: change fetched quantity

--- store/currencies-actions.ts
+++ store/currencies-actions.ts
@@ -8,7 +8,7 @@ export const fetchCurrenciesData = (isFirstLoading: boolean) => {
     if (isFirstLoading) {
       dispatch(uiActions.showLoading(true));
     }
-    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`;
+    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`;
 
     const fetchData = async () => {
       const response = await fetch(url);
