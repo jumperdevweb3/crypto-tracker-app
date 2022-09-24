294e715d9ee9873f5af5f082d9ab55b04af0a776
skoczy01 <filip.skoczylas00@wp.pl>
2022-09-24 13:50:57 +0200

    refactor: add new classes and Link on coin click

--- components/cards/statsCard/StatsCoinCard.tsx
+++ components/cards/statsCard/StatsCoinCard.tsx
@@ -1,6 +1,8 @@
 import classes from "./StatsCoinCard.module.scss";
+import Link from "next/link";
 
 export const StatsCoinCard = (props: {
+  id: string;
   percentage: any;
   number: number;
   name: string;
@@ -11,15 +13,25 @@ export const StatsCoinCard = (props: {
     props.percentage <= 0
       ? `${classes.percentage} ${classes.decr}`
       : `${classes.percentage} ${classes.incr}`;
+  const breakWordClass =
+    getWordCount(props.name) >= 2
+      ? `${classes.name} ${classes["word-wrap"]}`
+      : `${classes.name} ${classes["word-break"]}`;
+
+  function getWordCount(str: string) {
+    return str.split(" ").filter((n) => n != "").length;
+  }
   return (
     <div className={classes["curr-box"]}>
       <div className={classes["curr-list"]}>
         <span className={classes.rank}>{props.number}</span>
-        <div className={classes["coin-wrapper"]}>
-          <img src={props.image} />
-          <p className={classes.name}>{props.name}</p>
-        </div>
-        <span className={classes.alias}>{props.alias.toUpperCase()}</span>
+        <Link href={`/currency/${props.id}`}>
+          <div className={classes["coin-wrapper"]}>
+            <img src={props.image} />
+            <p className={breakWordClass}>{props.name}</p> -
+            <span className={classes.alias}>{props.alias.toUpperCase()}</span>
+          </div>
+        </Link>
       </div>
       <div className={valueStyle}>
         <p>{props.percentage.toFixed(2)}%</p>
