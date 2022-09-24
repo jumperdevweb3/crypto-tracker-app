a98cad0641027622c53d8d82f73aad463e214499
skoczy01 <filip.skoczylas00@wp.pl>
2022-09-24 13:52:57 +0200

    refactor: remove conditional class from modal

--- pages/currency/[coinId].tsx
+++ pages/currency/[coinId].tsx
@@ -3,17 +3,24 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../store/store";
 import { useRouter } from "next/router";
 import { CurrencyItem } from "../../types/types";
+//types
+import { Path } from "../../types/types";
 
 export default function CoinDetailPage() {
   const items = useSelector((state: RootState) => state.currencies.items);
 
-  type Path = string | string[] | undefined;
-
   const router = useRouter();
   const idPath: Path = router.query.coinId;
   const item: CurrencyItem | any = items.find((item) => item.id === idPath);
+  let statsModal: any;
+  if (process.browser) {
+    statsModal = document.getElementById("stats-modal");
+    if (router.pathname !== "/") {
+      statsModal.classList.remove("show");
+    }
+  }
 
-  if (!item) return <p>Not found</p>;
+  if (!item) return <p className="center">Coin Not found</p>;
 
   return <>{items.length !== 0 && <CurrenciesDetail item={item} />}</>;
 }
