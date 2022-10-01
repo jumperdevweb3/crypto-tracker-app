8b2fcde0d230b2cc26b26224da5f8086a1b5e566
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-01 17:40:33 +0200

    refactor: separating part of the code into a new component

--- components/currencies/currencyDetail/CurrenciesDetail.tsx
+++ components/currencies/currencyDetail/CurrenciesDetail.tsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import { fetchChartData } from "../../../store/currencies-actions";
 import classes from "./CurrenciesDetail.module.scss";
 import { LoadingSpinner } from "../../ui/LoadingSpinner";
+import { CurrencyStats } from "./CurrencyStats";
 
 const TradingViewChart = dynamic(() => import("./TradingViewChart"), {
   ssr: false,
@@ -17,121 +18,30 @@ export const CurrenciesDetail = ({ item }: { item: CurrencyItem }) => {
     (state: RootState) => state.currencies
   );
 
-  const {
-    id,
-    image,
-    name,
-    symbol,
-    current_price,
-    market_cap_rank,
-    price_change_24h,
-    market_cap,
-    ath,
-    ath_change_percentage,
-    last_updated,
-    price_change_1h,
-    price_change_7d,
-    total_volume,
-  } = item;
-  const interNumberFormat = new Intl.NumberFormat("en-US");
-
-  const timeStyle =
-    price_change_24h <= 0
-      ? `${classes.time} ${classes.decr}`
-      : `${classes.time} ${classes.incr}`;
-  const timeStyleAth =
-    ath_change_percentage <= 0
-      ? `${classes.time} ${classes.decr}`
-      : `${classes.time} ${classes.incr}`;
-
   useEffect(() => {
-    dispatch(fetchChartData(id));
+    dispatch(fetchChartData(item.id));
   }, [dispatch]);
 
   return (
     <>
       <div className={classes.container}>
-        <div className={classes["box-container"]}>
-          <div className={classes.box}>
-            <div className={classes.data}>
-              <img src={image} />
-              <p>{name}</p>
-              <p>{symbol.toUpperCase()}</p>
-            </div>
-
-            <p className={classes.rank}>Rank #{market_cap_rank}</p>
-            <div className={classes["second-data"]}>
-              <p className={classes.price}>
-                {interNumberFormat.format(current_price) === "0"
-                  ? current_price.toFixed(7)
-                  : interNumberFormat.format(current_price)}
-                $
+        <CurrencyStats item={item} />
+        <div id="chart">
+          {chartIsUpdating ? (
+            <LoadingSpinner />
+          ) : chartData.length !== 0 ? (
+            <>
+              <TradingViewChart chartData={chartData} />
+              <p className={classes["chart-info"]}>
+                The chart used comes from{" "}
+                <a href="https://www.tradingview.com/">
+                  https://www.tradingview.com/
+                </a>
               </p>
-              <p className={timeStyle}>{price_change_24h.toFixed(2)}%</p>
-            </div>
-          </div>
-          <div className={classes["detail-box"]}>
-            <p>
-              Market Cap: <span>${interNumberFormat.format(market_cap)}</span>
-            </p>
-            <p>
-              All Time High:{" "}
-              <span>
-                $
-                {interNumberFormat.format(ath) === "0"
-                  ? ath.toFixed(7)
-                  : interNumberFormat.format(ath)}
-              </span>
-            </p>
-            <p>
-              Price change from ATH:
-              <span className={timeStyleAth}>
-                {interNumberFormat.format(ath_change_percentage)} %
-              </span>
-            </p>
-            <p>
-              Last update date (UTC):
-              <span className={classes.date}>
-                {last_updated.replace(
-                  /(\d{4})-(\d{2})-(\d{2})T(.{8}).*/,
-                  "$2.$3.$1, $4"
-                )}
-              </span>
-            </p>
-            <p>
-              Price change 1h:
-              <span>{interNumberFormat.format(price_change_1h)} %</span>
-            </p>
-            <p>
-              Price change 24h:
-              <span>{interNumberFormat.format(price_change_24h)} %</span>
-            </p>
-            <p>
-              Price change 7d:
-              <span>{interNumberFormat.format(price_change_7d)} %</span>
-            </p>
-            <p>
-              Total volume:
-              <span>{interNumberFormat.format(total_volume)}</span>
-            </p>
-          </div>
-          <div id="chart">
-            {chartIsUpdating ? (
-              <LoadingSpinner />
-            ) : chartData.length !== 0 ? (
-              <>
-                <TradingViewChart chartData={chartData} />
-                <p className={classes["chart-info"]}>
-                  The chart is library used from{" "}
-                  <a href="https://www.tradingview.com/">
-                    https://www.tradingview.com/
-                  </a>
-                </p>
-              </>
-            ) : (
-              <p className="center">Faild load chart</p>
-            )}
-          </div>
+            </>
+          ) : (
+            <p className="center">Faild load chart</p>
+          )}
         </div>
       </div>
     </>
