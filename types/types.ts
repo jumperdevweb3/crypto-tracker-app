6cb65f98c96ddb0fcff3a16114f80a4701b47fd2
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-03 20:16:56 +0200

    refactor: add new types for statistic subpage data

--- types/types.ts
+++ types/types.ts
@@ -46,6 +46,7 @@ export interface WatchlistState {
 export interface ModalTypes {
   onClose: () => void;
   children: ReactNode;
+  id: string;
 }
 
 export interface NewsItems {
@@ -76,4 +77,80 @@ export type Path = string | string[] | undefined;
 
 export interface IParams extends ParsedUrlQuery {
   id: string;
+}
+
+export interface CompaniesType {
+  symbol: string;
+  country: string;
+  name: string;
+  total_holdings: number;
+  total_entry_value_usd: number;
+  total_current_value_usd: number;
+  percentage_of_total_supply: number;
+}
+
+export interface ExchangeType {
+  id: string;
+  name: string;
+  year_established: number;
+  country: string;
+  description: string;
+  url: string;
+  image: string;
+  trust_score: number;
+  trust_score_rank: number;
+  trade_volume_24h_btc: number;
+  trade_volume_24h_btc_normalized: number;
+}
+
+export interface NftsListTypes {
+  id: string;
+  contract_address: string;
+  name: string;
+  asset_platform_id: string;
+  symbol: string;
+}
+
+export interface NftTypes {
+  id: string;
+  contract_address: string;
+  asset_platform_id: string;
+  name: string;
+  image: {
+    small: string;
+  };
+  description: string;
+  native_currency: string;
+  floor_price: {
+    native_currency: number;
+    usd: number;
+  };
+  market_cap: {
+    native_currency: number;
+    usd: number;
+  };
+  volume_24h: {
+    native_currency: number;
+    usd: number;
+  };
+  number_of_unique_addresses: number;
+  number_of_unique_addresses_24h_percentage_change: number;
+  floor_price_in_usd_24h_percentage_change: number;
+  total_supply: number;
+}
+
+export interface StatisticTypes {
+  exchanges: {
+    items: ExchangeType[] | [];
+    errorMessage: string;
+  };
+  companies: {
+    items: CompaniesType[] | [];
+    errorMessage: string;
+  };
+  nfts: {
+    items: NftsListTypes[] | [];
+    errorMessage: string;
+  };
+  isLoading: boolean;
 }
