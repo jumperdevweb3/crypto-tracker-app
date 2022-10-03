75c1177e000fdb6cce315863ac92a3da6a9ebdcd
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-03 21:08:27 +0200

    refactor: add valdiation

--- components/cards/coinCard/PriceTimeChange.tsx
+++ components/cards/coinCard/PriceTimeChange.tsx
@@ -8,14 +8,18 @@ export const PriceTimeChange = ({
   classes: any;
 }) => {
   const timeStyle =
-    time <= 0
+    time && time <= 0
       ? `${classes.time} ${classes.decr}`
       : `${classes.time} ${classes.incr}`;
   const timeIcon = time <= 0 ? <FaAngleDown /> : <FaAngleUp />;
   return (
     <div className={timeStyle}>
-      <p>{Math.abs(+time.toFixed(2))}%</p>
-      {timeIcon}
+      {time && (
+        <>
+          <p>{Math.abs(+time.toFixed(2))}%</p>
+          {timeIcon}
+        </>
+      )}
     </div>
   );
 };
