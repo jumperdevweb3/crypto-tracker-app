28e65bfd01e08a8c4dc7079c4bb465be2df9d176
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-03 20:18:06 +0200

    feat: add a new subpage

---
+++ pages/statistics/index.tsx
@@ -0,0 +1,5 @@
+import { Statistics } from "../../components/statistic/Statistics";
+
+export default function StatisticPage() {
+  return <Statistics />;
+}
---
+++ pages/tools/index.tsx
@@ -0,0 +1,11 @@
+import { Converter } from "../../components/converter/Converter";
+import { Tracker } from "../../components/walletTracker/Tracker";
+
+export default function ToolsPage() {
+  return (
+    <div>
+      <Converter />
+      <Tracker />
+    </div>
+  );
+}
