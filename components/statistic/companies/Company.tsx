18fe380570e742b01414da92969b2647738ffd9f
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-03 20:15:29 +0200

    feat: add new component with content for modal

---
+++ components/statistic/companies/Company.tsx
@@ -0,0 +1,43 @@
+import { CompaniesType } from "../../../types/types";
+import classes from "../exchanges/Exchange.module.scss";
+
+export const Company = ({ ...props }) => {
+  const {
+    name,
+    symbol,
+    country,
+    total_holdings,
+    total_entry_value_usd,
+    total_current_value_usd,
+    percentage_of_total_supply,
+  } = props.item as CompaniesType;
+
+  return (
+    <div className={classes.container}>
+      <div className={classes["name-box"]}>
+        <p>{name} </p>
+      </div>
+      <div className={classes["data-box"]}>
+        <p>
+          Symbol: <span>{symbol}</span>
+        </p>
+        <p>
+          Country: <span>{country}</span>
+        </p>
+        <p>
+          Total holdings: <span>{total_holdings} BTC</span>
+        </p>
+        <p className={classes.volume}>
+          Percentage of total supply:{" "}
+          <span>{percentage_of_total_supply.toFixed(4)} BTC</span>
+        </p>
+        <p>
+          Total entry value USD: <span>{total_entry_value_usd}$</span>
+        </p>
+        <p>
+          Total Current value USD: <span>{total_current_value_usd}$</span>
+        </p>
+      </div>
+    </div>
+  );
+};
