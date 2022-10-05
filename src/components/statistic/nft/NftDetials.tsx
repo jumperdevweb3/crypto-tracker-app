64afda2559e077872153506978f87a5980670bd4
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-05 17:50:43 +0200

    feat: add validation render and loading spinner when data are fetching

--- src/components/statistic/nft/NftDetials.tsx
+++ src/components/statistic/nft/NftDetials.tsx
@@ -1,76 +1,104 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../store/store";
 import { NftTypes } from "../../../types/types";
+import { LoadingSpinner } from "../../ui/LoadingSpinner";
 import classes from "./NftDetails.module.scss";
+import { fetchNftDetial } from "../../../store/statistic-actions";
 
 export const NftDetials = ({ ...props }) => {
-  const {
-    name,
-    contract_address,
-    asset_platform_id,
-    image,
-    description,
-    native_currency,
-    floor_price,
-    market_cap,
-    volume_24h,
-    total_supply,
-    floor_price_in_usd_24h_percentage_change,
-    number_of_unique_addresses,
-    number_of_unique_addresses_24h_percentage_change,
-  } = props as NftTypes;
+  const dispatch = useDispatch<AppDispatch>();
+  const isLoadingDetial = useSelector(
+    (state: RootState) => state.statistic.isLoading.nftDetial
+  );
+  const [item, setItem] = useState<null | NftTypes>(null);
+
+  useEffect(() => {
+    fetchItem(props.id);
+  }, []);
+
+  const fetchItem = async (id: string) => {
+    const item = await dispatch(fetchNftDetial(id));
+    if (item && item !== undefined) {
+      setItem(item);
+    }
+    if (!item || item === undefined) {
+      setItem(null);
+    }
+  };
+
   return (
-    <div className={classes.container}>
-      <div className={classes["name-box"]}>
-        {image.small && <img src={image.small} alt={name + "logo"} />}
-        <p>{name}</p>
-      </div>
-      <div className={classes["content-box"]}>
-        <div className={classes["data-box"]}>
-          <p>
-            Native Currency: <span>{native_currency}</span>
-          </p>
-          <p>
-            Total Supply: <span>{total_supply}</span>
-          </p>
-          <p className={classes.contract}>
-            Contract Address : <span>{contract_address}</span>
-          </p>
-          <p className={classes.description}>{description}</p>
+    <>
+      {isLoadingDetial && <LoadingSpinner />}
+      {item && (
+        <div className={classes.container}>
+          <div className={classes["name-box"]}>
+            {item.image.small && (
+              <img src={item.image.small} alt={item.name + "logo"} />
+            )}
+            <p>{item.name}</p>
+          </div>
+          <div className={classes["content-box"]}>
+            <div className={classes["data-box"]}>
+              <p>
+                Native Currency: <span>{item.native_currency}</span>
+              </p>
+              <p>
+                Total Supply: <span>{item.total_supply}</span>
+              </p>
+              <p className={classes.contract}>
+                Contract Address : <span>{item.contract_address}</span>
+              </p>
+              <p className={classes.description}>{item.description}</p>
+            </div>
+            <div className={classes["stats-box"]}>
+              <p>
+                Floor Price:{" "}
+                <span>
+                  {item.floor_price.native_currency} ETH /{" "}
+                  {item.floor_price.usd} $
+                </span>
+              </p>
+              <p>
+                Market Cap:{" "}
+                <span>
+                  {item.market_cap.native_currency} ETH / {item.market_cap.usd}{" "}
+                  $
+                </span>
+              </p>
+              <p>
+                Volume 24h:{" "}
+                <span>
+                  {item.volume_24h.native_currency} ETH / {item.volume_24h.usd}{" "}
+                  $
+                </span>
+              </p>
+              <p>
+                Number of unique addresses:{" "}
+                <span>{item.number_of_unique_addresses}</span>
+              </p>
+              <p>
+                Unique addresses 24h change:{" "}
+                <span>
+                  {item.number_of_unique_addresses_24h_percentage_change.toFixed(
+                    3
+                  )}{" "}
+                  %
+                </span>
+              </p>
+              <p>
+                Floor Price Change in 24h:{" "}
+                <span>
+                  {item.floor_price_in_usd_24h_percentage_change.toFixed(3)} %
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
-        <div className={classes["stats-box"]}>
-          <p>
-            Floor Price:{" "}
-            <span>
-              {floor_price.native_currency} ETH / {floor_price.usd} $
-            </span>
-          </p>
-          <p>
-            Market Cap:{" "}
-            <span>
-              {market_cap.native_currency} ETH / {market_cap.usd} $
-            </span>
-          </p>
-          <p>
-            Volume 24h:{" "}
-            <span>
-              {volume_24h.native_currency} ETH / {volume_24h.usd} $
-            </span>
-          </p>
-          <p>
-            Number of unique addresses:{" "}
-            <span>{number_of_unique_addresses}</span>
-          </p>
-          <p>
-            Unique addresses 24h change:{" "}
-            <span>
-              {number_of_unique_addresses_24h_percentage_change.toFixed(3)} %
-            </span>
-          </p>
-          <p>
-            Floor Price Change in 24h:{" "}
-            <span>{floor_price_in_usd_24h_percentage_change.toFixed(3)} %</span>
-          </p>
-        </div>
-      </div>
-    </div>
+      )}
+      {!item && !isLoadingDetial && (
+        <p className="center">Problem with fetch data.</p>
+      )}
+    </>
   );
 };
