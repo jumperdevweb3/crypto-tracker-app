7cb9d8d5e0afa4803c5992a83d775e2043150afe
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-06 15:55:05 +0200

    refactor: change currency format

--- src/components/cards/coinCard/CoinCard.tsx
+++ src/components/cards/coinCard/CoinCard.tsx
@@ -10,8 +10,10 @@ import { AppDispatch } from "../../../store/store";
 
 export const CoinCard = ({ item }: { item: CurrencyItem }) => {
   const dispatch = useDispatch<AppDispatch>();
-  const interNumberFormat = new Intl.NumberFormat("en-US");
-
+  const interNumberFormat = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
   const addToWatchlistHandler = () => {
     dispatch(watchlistActions.updateItems(item));
   };
@@ -38,9 +40,8 @@ export const CoinCard = ({ item }: { item: CurrencyItem }) => {
       </div>
       <div className={classes.price}>
         <p>
-          $
-          {interNumberFormat.format(item.current_price) === "0"
-            ? item.current_price.toFixed(9)
+          {interNumberFormat.format(item.current_price) === "$0.00"
+            ? "$" + item.current_price.toFixed(9)
             : interNumberFormat.format(item.current_price)}
         </p>
       </div>
@@ -48,10 +49,14 @@ export const CoinCard = ({ item }: { item: CurrencyItem }) => {
       <PriceTimeChange time={item.price_change_24h} classes={classes} />
       <PriceTimeChange time={item.price_change_7d} classes={classes} />
       <div className={classes["market-cup"]}>
-        <p>${interNumberFormat.format(item.market_cap)}</p>
+        <p>
+          {interNumberFormat.format(item.market_cap) === "$0.00"
+            ? item.market_cap.toFixed(7)
+            : interNumberFormat.format(item.market_cap)}
+        </p>
       </div>
       <div className={classes.volume}>
-        <p>${interNumberFormat.format(item.total_volume)}</p>
+        <p>{interNumberFormat.format(item.total_volume)}</p>
       </div>
     </div>
   );
