1b65f609da349a1c58671b163aa788d828b6a4f2
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-07 15:38:02 +0200

    Refactor: moving links to a separate file

--- src/components/layout/MainNav.tsx
+++ src/components/layout/MainNav.tsx
@@ -3,25 +3,16 @@ import classes from "./MainNav.module.scss";
 import { uiActions } from "../../store/ui-slice";
 import { useDispatch, useSelector } from "react-redux";
 import { useWindowSize } from "../../hooks/use-windowSize";
-import { useRouter } from "next/router";
 import { FiMenu } from "react-icons/fi";
 //types
 import { AppDispatch } from "../../store/store";
 import { RootState } from "../../store/store";
+import { NavLinks } from "./NavLinks";
 
 export const MainNav = () => {
   const showNav = useSelector((state: RootState) => state.uiSlice.showNav);
   const dispatch = useDispatch<AppDispatch>();
   const { width } = useWindowSize();
-  const router = useRouter();
-
-  const links = [
-    { name: "Home", path: "/" },
-    { name: "Watchlist", path: "/watchlist" },
-    { name: "Statistics", path: "/statistics" },
-    { name: "Tools", path: "/tools" },
-    { name: "News", path: "/news" },
-  ];
 
   const navClass = showNav
     ? `${classes["mobile-nav"]} ${classes.active}`
@@ -33,21 +24,6 @@ export const MainNav = () => {
   const toggleNav = () => {
     dispatch(uiActions.showNavigation(!showNav));
   };
-
-  const navList = links.map((item) => {
-    return (
-      <li key={item.name}>
-        <Link href={item.path}>
-          <a
-            className={router.pathname === item.path ? classes.active : " "}
-            onClick={toggleNav}
-          >
-            {item.name}
-          </a>
-        </Link>
-      </li>
-    );
-  });
   return (
     <header className={classes.header}>
       <div className={classes.wrapper}>
@@ -63,13 +39,17 @@ export const MainNav = () => {
         </div>
         {mobile && (
           <nav className={navClass}>
-            <ul className={classes["mobile-list"]}>{navList}</ul>
+            <ul className={classes["mobile-list"]}>
+              <NavLinks toggle={toggleNav} />
+            </ul>
           </nav>
         )}
 
         {desktop && (
           <nav className={classes["desktop-nav"]}>
-            <ul className={classes["links-list"]}>{navList}</ul>
+            <ul className={classes["links-list"]}>
+              <NavLinks toggle={toggleNav} />
+            </ul>
           </nav>
         )}
       </div>
