cbf4fdda50f2a895053bdd2e197d8a96da19a70d
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-07 15:54:43 +0200

    Refactor: create component of the rendered list of items

---
+++ src/components/statistic/nft/NftsList.tsx
@@ -0,0 +1,23 @@
+import { NftsListTypes } from "../../../types/types";
+import classes from "./NftList.module.scss";
+
+interface Props {
+  items: NftsListTypes[];
+  modalAction: (id: string) => void;
+}
+
+export const NftsList = ({ items, modalAction }: Props) => {
+  const renderItems = items.map((item, index) => (
+    <li
+      key={item.id}
+      className={classes["list-item"]}
+      onClick={() => modalAction(item.id)}
+    >
+      <p className={classes.name}>
+        {index + 1}. {item.name}
+      </p>
+      <span className={classes.symbol}>{item.symbol}</span>
+    </li>
+  ));
+  return <>{renderItems}</>;
+};
