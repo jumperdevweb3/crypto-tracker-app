dcce175082552a35715d6bd5c52930eda3715f05
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-07 15:37:05 +0200

    Refactor: separate component for page links

---
+++ src/components/layout/links.ts
@@ -0,0 +1,7 @@
+export const links = [
+  { name: "Home", path: "/" },
+  { name: "Watchlist", path: "/watchlist" },
+  { name: "Statistics", path: "/statistics" },
+  { name: "Tools", path: "/tools" },
+  { name: "News", path: "/news" },
+];
