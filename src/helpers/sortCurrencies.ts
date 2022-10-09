39efb7354e5eb4ed7235998d3a199504ade0dfb0
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-09 12:32:38 +0200

    Refactor: move sort function to separate component

---
+++ src/helpers/sortCurrencies.ts
@@ -0,0 +1,28 @@
+import { CurrencyItem } from "../types/types";
+
+export const sortCurrencies = (
+  items: CurrencyItem[],
+  { sortType, sortBy }: { sortType: string; sortBy: string }
+) => {
+  if (sortType === "ascending") {
+    if (sortBy === "name") {
+      return [...items].sort((a: any, b: any): any =>
+        (b[sortBy] || "").toString().localeCompare((a[sortBy] || "").toString())
+      );
+    }
+    return [...items].sort(
+      (a, b) => a[sortBy as keyof {}] - b[sortBy as keyof {}]
+    );
+  }
+  if (sortType === "descending") {
+    if (sortBy === "name") {
+      return [...items].sort((a: any, b: any): any =>
+        (a[sortBy] || "").toString().localeCompare((b[sortBy] || "").toString())
+      );
+    }
+    return [...items].sort(
+      (a, b) => b[sortBy as keyof {}] - a[sortBy as keyof {}]
+    );
+  }
+  return items;
+};
