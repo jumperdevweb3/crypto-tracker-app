1959454a42eb995cd9b37d5bb922cfc1ff3b43e2
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-09 13:36:19 +0200

    Feat: add author link

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -3,7 +3,10 @@ import classes from "./Footer.module.scss";
 export const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <a href="https://github.com/skoczy01">Check my GitHub</a>
+      <a href="https://github.com/skoczy01/crypto-tracker-app">
+        Project GitHub
+      </a>
+      <a href="https://github.com/skoczy01">Author</a>
     </footer>
   );
 };
