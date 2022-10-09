39a5f1f8edf9303b962efec515ca823b3226dc82
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-09 13:36:02 +0200

    Feat: add footer for each page

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,12 +2,14 @@ import { MainNav } from "./MainNav";
 import classes from "./Layout.module.scss";
 import { PropsChildren } from "../../types/types";
 import { RootActions } from "../RootActions";
+import { Footer } from "../ui/Footer";
 
 export const Layout = ({ children }: PropsChildren) => {
   return (
     <RootActions>
       <MainNav />
       <main className={classes.main}>{children}</main>
+      <Footer />
     </RootActions>
   );
 };
