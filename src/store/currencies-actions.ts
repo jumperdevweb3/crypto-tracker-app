7cc43e26728de04450a275f90321100f2b037d44
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-09 12:34:14 +0200

    Refactor: change fetch functions to custom fetch & import extracted url's

--- src/store/currencies-actions.ts
+++ src/store/currencies-actions.ts
@@ -2,34 +2,29 @@ import { currenciesActions } from "./currencies-slice";
 import { uiActions } from "./ui-slice";
 import { scannerActions } from "./etherscan-slice";
 import { AppDispatch } from "./store";
-import { CurrencyItem } from "../types/types";
+import { urlFetchList } from "../helpers/urlFetchList";
+import { useFetchData } from "../helpers/fetchData";
 
 export const fetchCurrenciesData = (isFirstLoading: boolean) => {
   return async (dispatch: AppDispatch) => {
     if (isFirstLoading) {
       dispatch(uiActions.showLoading(true));
     }
-    const url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`;
-
-    const fetchData = async () => {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error("Could not fetch cart data!");
-      }
-      const data = await response.json();
-      if (isFirstLoading) {
-        dispatch(uiActions.showLoading(false));
-      }
-      return data;
-    };
+    const fetchData = useFetchData({
+      url: urlFetchList.currenciesList,
+      message: "Currencies Data",
+    });
     try {
-      const currenciesData = await fetchData();
+      const currenciesData = await fetchData;
       dispatch(
         uiActions.showNotification({
           message: "",
         })
       );
       dispatch(currenciesActions.setItems(currenciesData));
+      if (isFirstLoading) {
+        dispatch(uiActions.showLoading(false));
+      }
     } catch (error) {
       dispatch(uiActions.showLoading(false));
       dispatch(
@@ -44,19 +39,16 @@ export const fetchCurrenciesData = (isFirstLoading: boolean) => {
 export const fetchChartData = (id: string) => {
   return async (dispatch: AppDispatch) => {
     dispatch(currenciesActions.setLoading(true));
-    const fetchData = async () => {
-      const response = await fetch(
-        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=450&interval=daily`
-      );
-      if (!response.ok) {
-        throw new Error("Could not fetch chart data, try later.");
-      }
-      const data = await response.json();
-
-      return data;
-    };
+    const fetchData = useFetchData({
+      url: `${
+        urlFetchList.chartData.firstPhrase +
+        id +
+        urlFetchList.chartData.secondPhrase
+      }`,
+      message: "Chart Data",
+    });
     try {
-      const chartData = await fetchData();
+      const chartData = await fetchData;
       dispatch(currenciesActions.setChart(chartData.prices));
       dispatch(currenciesActions.setLoading(false));
     } catch (error) {
@@ -68,22 +60,13 @@ export const fetchChartData = (id: string) => {
 
 export const fetchEtherScanData = (address: string) => {
   return async (dispatch: AppDispatch) => {
-    const fetchData = async () => {
-      dispatch(scannerActions.setIsLoading(true));
-      const response = await fetch(
-        `https://api.etherscan.io/api?module=account&action=balance&address=${address}&tag=latest&apikey=${process.env.REACT_APP_SCAN_API}`
-      );
-      if (!response.ok) {
-        throw new Error(
-          "Could not fetch ether scan data, try later or slow down (5 req per sec)."
-        );
-      }
-      const data = await response.json();
-
-      return data;
-    };
+    dispatch(scannerActions.setIsLoading(true));
+    const fetchData = useFetchData({
+      url: `${urlFetchList.etherScan + address}`,
+      message: "Ether Scan",
+    });
     try {
-      const scanData = await fetchData();
+      const scanData = await fetchData;
       if (scanData.status === "1") {
         dispatch(scannerActions.setErrorMsg(""));
         dispatch(scannerActions.setResult(scanData.result));
@@ -100,32 +83,6 @@ export const fetchEtherScanData = (address: string) => {
   };
 };
 
-export const sortCurrencies = (
-  items: CurrencyItem[],
-  { sortType, sortBy }: { sortType: string; sortBy: string }
-) => {
-  if (sortType === "ascending") {
-    if (sortBy === "name") {
-      return [...items].sort((a: any, b: any): any =>
-        (b[sortBy] || "").toString().localeCompare((a[sortBy] || "").toString())
-      );
-    }
-    return [...items].sort(
-      (a, b) => a[sortBy as keyof {}] - b[sortBy as keyof {}]
-    );
-  }
-  if (sortType === "descending") {
-    if (sortBy === "name") {
-      return [...items].sort((a: any, b: any): any =>
-        (a[sortBy] || "").toString().localeCompare((b[sortBy] || "").toString())
-      );
-    }
-    return [...items].sort(
-      (a, b) => b[sortBy as keyof {}] - a[sortBy as keyof {}]
-    );
-  }
-  return items;
-};
 //in future
 // export const fetchFiatCurrencies = () => {
 //   return async (dispatch) => {
