5c02fbb70de9008b102734d2d07da4cf7bd8deac
skoczy01 <skoczy01@wp.pl>
2022-10-13 14:43:12 +0200

    Refactor: Wrap list-item content in Link  & add 'a' element

--- src/components/currencies/searchBar/SearchItem.tsx
+++ src/components/currencies/searchBar/SearchItem.tsx
@@ -4,16 +4,18 @@ import classes from "./SearchBar.module.scss";
 
 export const SearchItem = ({ ...item }: CurrencyItem) => {
   return (
-    <Link href={`/currency/${item.id}`}>
-      <li className={classes["list-item"]}>
-        <div className={classes["name-data"]}>
-          <img src={item.image} alt={item.name} />
-          <p>
-            {item.name} <span>{item.symbol.toUpperCase()}</span>
-          </p>
-        </div>
-        <span>#{item.market_cap_rank}</span>
-      </li>
-    </Link>
+    <li>
+      <Link href={`/currency/${item.id}`}>
+        <a className={classes["list-item"]}>
+          <div className={classes["name-data"]}>
+            <img src={item.image} alt={item.name} />
+            <p>
+              {item.name} <span>{item.symbol.toUpperCase()}</span>
+            </p>
+          </div>
+          <span>#{item.market_cap_rank}</span>
+        </a>
+      </Link>
+    </li>
   );
 };
