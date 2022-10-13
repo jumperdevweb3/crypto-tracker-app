ee8f4ce29c5a447cf71dd1e7fc9000527f6e3322
skoczy01 <skoczy01@wp.pl>
2022-10-13 14:46:03 +0200

    Refactor: Wrap 'a' element to nextjs Link

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,12 +1,15 @@
+import Link from "next/link";
 import classes from "./Footer.module.scss";
 
 export const Footer = () => {
   return (
     <footer className={classes.footer}>
-      <a href="https://github.com/skoczy01/crypto-tracker-app">
-        Project GitHub
-      </a>
-      <a href="https://github.com/skoczy01">Author</a>
+      <Link href="https://github.com/skoczy01/crypto-tracker-app" passHref>
+        <a target="_blank">Project GitHub</a>
+      </Link>
+      <Link href="https://github.com/skoczy01" passHref>
+        <a target="_blank">Author</a>
+      </Link>
     </footer>
   );
 };
