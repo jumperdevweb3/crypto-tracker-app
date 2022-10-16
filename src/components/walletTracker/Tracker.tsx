fec4ab10719bb7e9f0206253d8591b4c7ddf409c
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-16 20:40:07 +0200

    Refactor: Add local state instead redux slice

--- src/components/walletTracker/Tracker.tsx
+++ src/components/walletTracker/Tracker.tsx
@@ -1,34 +1,44 @@
 import classes from "./Tracker.module.scss";
 import { FormEvent, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { FaSearch } from "react-icons/fa";
-import { fetchEtherScanData } from "../../store/currencies-actions";
 import { LoadingSpinner } from "../ui/LoadingSpinner";
-//types
-import { AppDispatch } from "../../store/store";
-import { RootState } from "../../store/store";
+import { fetchEtherScanData } from "../utils/api-utils";
 
 export const Tracker = () => {
-  const {
-    isLoading,
-    errorMessage: fetchErrorMessage,
-    result: fetchResult,
-  } = useSelector((state: RootState) => state.scanner);
-
+  const [result, setResult] = useState({
+    amount: 0,
+    error: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const dispatch = useDispatch<AppDispatch>();
 
   const inputChangeHandler = (event: FormEvent<HTMLInputElement>) => {
     setInputValue(event.currentTarget.value);
   };
-  const fetchDataHandler = (event: FormEvent<HTMLFormElement>) => {
+  const fetchDataHandler = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    if (!inputValue || inputValue.length < 18) return;
-    dispatch(fetchEtherScanData(inputValue));
+    setIsLoading(true);
+    if (!inputValue || inputValue.length < 18) {
+      setInputValue("");
+      setIsLoading(false);
+    }
+    const data = await fetchEtherScanData(inputValue);
+    if (data.status === "0") {
+      setResult({
+        amount: 0,
+        error: data.result,
+      });
+    }
+    if (data.status === "1") {
+      const dataFormat = data.result * Math.pow(10, -18);
+      setResult({
+        amount: dataFormat,
+        error: "",
+      });
+    }
+    setIsLoading(false);
     setInputValue("");
   };
-
-  const result = fetchErrorMessage ? 0 : fetchResult * Math.pow(10, -18);
   return (
     <div className={classes.container}>
       <form className={classes.box} onSubmit={fetchDataHandler}>
@@ -45,15 +55,17 @@ export const Tracker = () => {
       </form>
       {isLoading && <LoadingSpinner />}
       <div className={classes.result}>
-        <p>
-          Entered Wallet have a{" "}
-          <span className={classes.amount}>
-            {result ? result.toFixed(10) : 0}{" "}
-          </span>
-          ETH
-        </p>
+        {result.amount ? (
+          <p>
+            Entered Wallet have a{" "}
+            <span className={classes.amount}>{result.amount.toFixed(10)}</span>{" "}
+            ETH
+          </p>
+        ) : (
+          "Search to show wallet ETH balance."
+        )}
       </div>
-      <p className={classes.error}>{fetchErrorMessage && fetchErrorMessage}</p>
+      {result.error && <p className={classes.error}>{result.error}</p>}
       <p className={classes.description}>
         *You can use test wallet address -
         0x0D992fF8cd5c417Ce6c935A6d36e027f91119Ccf
