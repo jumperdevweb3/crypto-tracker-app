fc6ae20478cf3baedb3c359f26c34ef760629d96
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-16 20:38:03 +0200

    Refactor: Move swap btn to separate component

--- src/components/converter/ConvertTools.tsx
+++ src/components/converter/ConvertTools.tsx
@@ -1,27 +1,13 @@
 import { ConvertItem } from "./ConvertItem";
-import { BiTransfer } from "react-icons/bi";
 import classes from "./Converter.module.scss";
 import { ConvertAmount } from "./ConvertAmount";
-import { useDispatch } from "react-redux";
-import { convertActions } from "../../store/convert-slice";
-//types
-import { AppDispatch } from "../../store/store";
+import { ConverSwapper } from "./ConverSwapper";
 export const ConvertTools = () => {
-  const dispatch = useDispatch<AppDispatch>();
-
-  const swapCurrenciesHandler = () => {
-    dispatch(convertActions.swap());
-  };
-
   return (
     <div className={classes["inputs-box"]}>
       <ConvertAmount />
       <ConvertItem kind="from" />
-      <div className={classes["convert-type"]}>
-        <button onClick={swapCurrenciesHandler}>
-          <BiTransfer fontSize="2rem" color="rgb(193, 162, 222)" />
-        </button>
-      </div>
+      <ConverSwapper />
       <ConvertItem kind="to" />
     </div>
   );
