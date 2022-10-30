2866ddd2ad747f9467e6d93243040aee8c9174c6
skoczy01 <filip.skoczylas00@wp.pl>
2022-10-30 12:34:33 +0100

    Feat: Create component for coin stats modal

---
+++ src/components/cards/trendingStatsCard/statsModal/StatsModal.tsx
@@ -0,0 +1,6 @@
+import { PropsChildren } from "../../../../types/types";
+import classes from "./StatsModal.module.scss";
+
+export const StatsModal = ({ children }: PropsChildren) => {
+  return <div className={classes["stats-modal"]}>{children}</div>;
+};
