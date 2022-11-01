395ffde8f023eb7ffdb368b3ce4e18608e472769
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-01 20:18:06 +0100

    Feat:Perf: use debounce for better performance

--- src/components/currencies/searchBar/SearchBar.tsx
+++ src/components/currencies/searchBar/SearchBar.tsx
@@ -1,21 +1,31 @@
 import classes from "./SearchBar.module.scss";
 import { InputSearch } from "../../ui/inputs/InputSearch";
 import { useSelector } from "react-redux";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RootState } from "../../../store/store";
 import { SearchItem } from "./SearchItem";
+import { useDebounce } from "./useDebounce";
+import { CurrencyItem } from "../../../types/types";
 
 export const SearchBar = () => {
   const [inputValue, setInputValue] = useState("");
+  const [searchItems, setSearchItems] = useState<CurrencyItem[]>([]);
+  const [loading, setLoading] = useState(false);
 
+  const debouncedSearch = useDebounce(inputValue, 500);
   const { items } = useSelector((state: RootState) => state.currencies);
 
-  const searchItems = inputValue
-    ? items.filter((coin) => {
+  useEffect(() => {
+    if (debouncedSearch) {
+      const filteredItems = items.filter((coin) => {
         const name = coin.name.toLowerCase();
         return name.includes(inputValue);
-      })
-    : [];
+      });
+      setSearchItems(filteredItems);
+    }
+    if (!debouncedSearch) setSearchItems([]);
+    setLoading(false);
+  }, [debouncedSearch]);
 
   const renderItems = searchItems
     .slice(0, 15)
@@ -24,6 +34,8 @@ export const SearchBar = () => {
   function inputValueHandler(event: React.ChangeEvent<HTMLInputElement>) {
     const newValue = event.target.value;
     setInputValue(newValue.trim().toLowerCase());
+    setSearchItems([]);
+    setLoading(true);
   }
   function onClickCross() {
     setInputValue("");
@@ -32,11 +44,12 @@ export const SearchBar = () => {
   const content = inputValue.length !== 0 && (
     <div className={classes["result-box"]}>
       <ul className={classes.list}>
-        {renderItems.length === 0 ? (
-          <li className={classes["not-found"]}>Not found items.</li>
+        {renderItems.length === 0 && !loading ? (
+          <li className={classes["result-info"]}>Not found items.</li>
         ) : (
           renderItems
         )}
+        {loading && <li className={classes["result-info"]}>Loading ...</li>}
       </ul>
     </div>
   );
