bfc2cf91b4e95e1f521800a5fbd10f524d6e806c
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-02 17:09:52 +0100

    Refactor: change variables structure

--- src/utils/api-utils.ts
+++ src/utils/api-utils.ts
@@ -1,9 +1,10 @@
 import { NewsItems } from "../types/types";
 import { urlFetchList } from "../helpers/urlFetchList";
 
+const { news, etherScan } = urlFetchList;
 export const fetchNewsData = async () => {
   try {
-    const response = await fetch(urlFetchList.newsList.news);
+    const response = await fetch(news.newsList);
     if (!response.ok) throw new Error("Fail fetch data");
     const data = await response.json();
     const items = data.data;
@@ -18,26 +19,9 @@ export const fetchNewsData = async () => {
   }
 };
 
-// export const fetchSubpageId = async () => {
-//   try {
-//     const response = await fetch(urlFetchList.newsList.newsSubpageId);
-//     if (!response.ok) throw new Error("Fail fetch data");
-//     const data = await response.json();
-//     const items = data.data;
-//     return items;
-//   } catch (error) {
-//     if (typeof error === "string") {
-//       console.log(error.toUpperCase());
-//     } else if (error instanceof Error) {
-//       console.log(error.message);
-//     }
-//     return [];
-//   }
-// };
-
 export const fetchNewsContent = async (id: string) => {
   try {
-    const response = await fetch(urlFetchList.newsList.newsContent);
+    const response = await fetch(news.newsContent);
     if (!response.ok) throw new Error("Fail fetch content data");
     const data = await response.json();
     const items = data.data;
@@ -60,7 +44,7 @@ export async function getNewsDetail(id: string) {
 
 export const fetchEtherScanData = async (address: string) => {
   try {
-    const response = await fetch(urlFetchList.etherScan + address);
+    const response = await fetch(etherScan + address);
     if (!response.ok) throw new Error("problem");
     const data = await response.json();
     return data;
