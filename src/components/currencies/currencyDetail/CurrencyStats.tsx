954543d53ec44bf27827fcfba85f2bb69c36b294
skoczy01 <skoczy01@wp.pl>
2022-11-04 14:54:29 +0100

    Feat: change variables for a new data

--- src/components/currencies/currencyDetail/CurrencyStats.tsx
+++ src/components/currencies/currencyDetail/CurrencyStats.tsx
@@ -3,32 +3,33 @@ import PriceTimeChange from "../../cards/coinCard/price/PriceTimeChange";
 import { currencyValueFormat } from "../../../helpers/numberFromat";
 import Price from "../../cards/coinCard/price/Price";
 //types
-import { CurrencyItem } from "../../../types/types";
 import Image from "next/image";
+import { Coin } from "../../../types/types";
+import { ReactNode } from "react";
 
-export const CurrencyStats = (props: { item: CurrencyItem }) => {
+interface Props {
+  item: Coin;
+  children?: ReactNode;
+}
+export const CurrencyStats = ({ item, children }: Props) => {
   const {
-    image,
-    name,
+    id,
     symbol,
-    current_price,
-    market_cap_rank,
-    price_change_24h,
-    market_cap,
-    ath,
-    ath_change_percentage,
-    last_updated,
-    price_change_1h,
-    price_change_7d,
-    total_volume,
-  } = props.item;
-
+    name,
+    description,
+    links,
+    genesis_date,
+    image,
+    community_data,
+    market_data,
+  } = item;
+  console.log(links);
   return (
     <div className={classes["box-container"]}>
       <div className={classes.header}>
         <div className={classes["main-title"]}>
           <Image
-            src={image}
+            src={image.thumb}
             alt={name}
             width={"21px"}
             height={"21px"}
@@ -38,7 +39,7 @@ export const CurrencyStats = (props: { item: CurrencyItem }) => {
           <p>{symbol.toUpperCase()}</p>
         </div>
 
-        <p className={classes.rank}>Rank #{market_cap_rank}</p>
+        <p className={classes.rank}>Rank #{market_data.market_cap_rank}</p>
 
         <div className={classes["price-data"]}>
           <p className={classes.head}>
@@ -46,9 +47,9 @@ export const CurrencyStats = (props: { item: CurrencyItem }) => {
           </p>
           <div className={classes["values-box"]}>
             <div className={classes.price}>
-              <Price price={current_price} />
+              <Price price={market_data.current_price.usd} />
             </div>
-            <PriceTimeChange time={price_change_24h} classes={classes} />
+            {/* <PriceTimeChange time={price_change_24h} classes={classes} /> */}
           </div>
         </div>
       </div>
@@ -57,24 +58,48 @@ export const CurrencyStats = (props: { item: CurrencyItem }) => {
         <div className={classes["first-box"]}>
           <div className={classes.wrapper}>
             <p> Market Cap:</p>{" "}
-            <span>{currencyValueFormat.format(market_cap)}</span>
+            <span>
+              {currencyValueFormat.format(market_data.market_cap.usd)}
+            </span>
+          </div>
+          <div className={classes.wrapper}>
+            <p>
+              Total supply:
+              <span>
+                {market_data.total_supply + " " + symbol.toUpperCase()}
+              </span>
+            </p>
+          </div>
+          <div className={classes.wrapper}>
+            <p>
+              Max supply:
+              <span>
+                {market_data.max_supply
+                  ? market_data.max_supply + " " + symbol.toUpperCase()
+                  : "-"}
+              </span>
+            </p>
           </div>
           <div className={classes.wrapper}>
             <p>All Time High:</p>
             <span>
-              {currencyValueFormat.format(ath) === "$0.00"
-                ? ath.toFixed(7)
-                : currencyValueFormat.format(ath)}
+              {currencyValueFormat.format(market_data.ath.usd) === "$0.00"
+                ? market_data.ath.usd.toFixed(7)
+                : currencyValueFormat.format(market_data.ath.usd)}
             </span>
           </div>
           <div className={classes.wrapper}>
             <p>Price change from ATH:</p>
-            <PriceTimeChange time={ath_change_percentage} classes={classes} />
+            <PriceTimeChange
+              time={market_data.ath_change_percentage.usd}
+              classes={classes}
+            />
           </div>
+
           <div className={classes["date-box"]}>
             <p>Last update date (UTC):</p>
             <span className={classes.date}>
-              {last_updated.replace(
+              {market_data.last_updated.replace(
                 /(\d{4})-(\d{2})-(\d{2})T(.{8}).*/,
                 "$2.$3.$1, $4"
               )}
@@ -83,21 +108,20 @@ export const CurrencyStats = (props: { item: CurrencyItem }) => {
         </div>
         <div className={classes["second-box"]}>
           <div className={classes.wrapper}>
-            <p>Price change 1h:</p>
-            <PriceTimeChange time={price_change_1h} classes={classes} />
+            <p>Links: </p>
+            {/* <div className={classes.links}>
+              {links.blockchain_site.map((i) => (
+                <a href={i}>{i}</a>
+              ))}
+            </div> */}
           </div>
           <div className={classes.wrapper}>
-            <p>Price change 24h:</p>
-            <PriceTimeChange time={price_change_24h} classes={classes} />
+            <p>?</p>
           </div>
           <div className={classes.wrapper}>
-            <p>Price change 7d:</p>
-            <PriceTimeChange time={price_change_7d} classes={classes} />
+            <p>?</p>
           </div>
-          <div className={classes.wrapper}>
-            <p>Total volume:</p>
-            <span>{currencyValueFormat.format(total_volume)}</span>
-          </div>
+          <div className={classes.wrapper}></div>
         </div>
       </div>
     </div>
