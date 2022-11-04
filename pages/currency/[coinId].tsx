76b9a01c91641ca690e4f4c390d72c85c2dad1a5
skoczy01 <skoczy01@wp.pl>
2022-11-04 14:53:23 +0100

    Feat: fetch currency instead filter in local

--- pages/currency/[coinId].tsx
+++ pages/currency/[coinId].tsx
@@ -1,19 +1,37 @@
 import { CurrenciesDetail } from "../../src/components/currencies/currencyDetail/CurrenciesDetail";
-import { useSelector } from "react-redux";
-import { RootState } from "../../src/store/store";
 import { useRouter } from "next/router";
-import { CurrencyItem } from "../../src/types/types";
+import { Coin } from "../../src/types/types";
 //types
-import { Path } from "../../src/types/types";
+import { useEffect, useState } from "react";
+import { fetchCurrency } from "../api/fetchCurrency";
+import { LoadingSpinner } from "../../src/components/ui/loadingSpinner/LoadingSpinner";
 
 export default function CoinDetailPage() {
-  const items = useSelector((state: RootState) => state.currencies.items);
-
+  const [item, setItem] = useState<Coin | null>();
+  const [isLoading, setIsLoading] = useState(false);
   const router = useRouter();
-  const idPath: Path = router.query.coinId;
-  const item: CurrencyItem | any = items.find((item) => item.id === idPath);
+  const idPath = router.query.coinId;
+  console.log(idPath);
 
-  if (!item) return <p className="center">Coin Not found</p>;
+  const fetchItem = async () => {
+    setIsLoading(true);
+    if (typeof idPath !== "object" && typeof idPath !== "undefined") {
+      const item = await fetchCurrency(idPath);
+      if (item.id) {
+        setItem(item);
+      } else {
+        setItem(null);
+      }
+      setIsLoading(false);
+      return;
+    }
+  };
+  useEffect(() => {
+    fetchItem();
+    console.log(item);
+  }, [idPath]);
 
-  return <>{items.length !== 0 && <CurrenciesDetail item={item} />}</>;
+  if (isLoading) return <LoadingSpinner />;
+  if (!item && !isLoading) return <p className="center">Coin Not found</p>;
+  return <>{item && <CurrenciesDetail item={item} />}</>;
 }
