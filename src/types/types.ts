bc1f58ef864e8f4ae843113aaa122ec263bf399d
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-06 18:22:54 +0100

    Refactor: change test type

--- src/types/types.ts
+++ src/types/types.ts
@@ -22,6 +22,7 @@ export interface CurrencyItem {
 }
 
 export interface CurrenciesState {
+  test: { [page: string]: CurrencyItem[] };
   items: CurrencyItem[];
   visibleItems: CurrencyItem[];
   trendingItems: CurrencyItem[];
