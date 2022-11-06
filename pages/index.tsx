e111916db1e519af9dbdebf68d207c471af93e96
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-06 18:24:22 +0100

    Feat: add test data

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,31 @@ import { SearchBar } from "../src/components/currencies/searchBar/SearchBar";
 //types
 import { AppDispatch } from "../src/store/store";
 import { RootState } from "../src/store/store";
+import { useRouter } from "next/router";
+import { currenciesActions } from "../src/store/currencies/currencies-slice";
 
 let isFirstLoading = true;
 function HomePage() {
+  const router = useRouter();
   const dispatch = useDispatch<AppDispatch>();
-  const currenciesData = useSelector(
-    (state: RootState) => state.currencies.items
-  );
+
+  const test = useSelector((state: RootState) => state.currencies.test);
+  const isHome = router.asPath === "/";
+  const isQuery = typeof router.query.page === "string" ? router.query.page : 1;
+
+  const page = isHome ? 1 : isQuery;
 
   useEffect(() => {
-    if (currenciesData.length === 0) {
-      dispatch(fetchCurrenciesData(isFirstLoading));
+    if (page in test) {
+      dispatch(
+        currenciesActions.setVisibleItems({
+          items: test[page],
+        })
+      );
+      return;
     }
-  }, [dispatch]);
+    dispatch(fetchCurrenciesData(false, page));
+  }, [dispatch, router]);
 
   return (
     <>
