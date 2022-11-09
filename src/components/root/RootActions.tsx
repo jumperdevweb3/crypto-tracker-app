6196bcb7dea0e9863777a43993d184e306d5d9ea
skoczy01 <skoczy01@wp.pl>
2022-11-09 15:02:05 +0100

    Refactor: remove part of comment

--- src/components/root/RootActions.tsx
+++ src/components/root/RootActions.tsx
@@ -1,27 +1,15 @@
 import { useEffect } from "react";
 import { AppDispatch, RootState } from "../../store/store";
-import { fetchCurrenciesData } from "../../store/currencies/currencies-actions";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { watchlistActions } from "../../store/watchlist/watchlist-slice";
 import { PropsChildren } from "../../types/types";
 import { TIME_TO_REFRESH_DATA } from "./fetchSettings";
-import { useRouter } from "next/router";
 
 export const RootActions = ({ children }: PropsChildren) => {
   const dispatch = useDispatch<AppDispatch>();
-  // const currenciesData = useSelector(
-  //   (state: RootState) => state.currencies.items
-  // );
-  const data = useSelector((state: RootState) => state.watchlist.watchItems);
-  const router = useRouter();
-  const key = router.asPath;
 
-  // useEffect(() => {
-  //   if (currenciesData.length === 0) {
-  //     dispatch(fetchCurrenciesData(false));
-  //   }
-  // }, [dispatch]);
+  const data = useSelector((state: RootState) => state.watchlist.watchItems);
 
   useEffect(() => {
     const local = localStorage.getItem("watchlist");
