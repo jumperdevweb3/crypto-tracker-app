f6f44e812445c51115081e00ad79b03f9eef6381
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-11 21:08:42 +0100

    Refactor: show trending stats

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import { CurrenciesList } from "../src/components/currencies/currenciesList/CurrenciesList";
 import { SearchBar } from "../src/components/currencies/searchBar/SearchBar";
+import { TrendingStatsBox } from "../src/components/currencies/trendingStats/TrendingStatsBox";
 //types
 
 function HomePage() {
   return (
     <>
-      {/* <TrendingStatsBox /> */}
+      <TrendingStatsBox />
       <SearchBar />
       <CurrenciesList />
     </>
