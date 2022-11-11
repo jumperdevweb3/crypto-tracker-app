d74f822879abcaf81207b6d82026a1b7fb914bd0
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-11 21:47:21 +0100

    Feat: add variables for query

--- src/components/currencies/currenciesList/getCurrencies.ts
+++ src/components/currencies/currenciesList/getCurrencies.ts
@@ -1,6 +1,10 @@
-export const getCurrenecies = async (page: number | string) => {
+export const getCurrenecies = async (
+  page: number | string,
+  perPage?: number
+) => {
+  const itemsPerPage = perPage ? perPage : 5;
   const res = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=5&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
+    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${itemsPerPage}&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
   );
   const data = res.json();
 
