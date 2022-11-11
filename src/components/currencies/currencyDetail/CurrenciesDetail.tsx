c125c05522ccd088fea54286fe1c3373eb2bef76
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-11 21:51:55 +0100

    Refactor: remove chart fetch

--- src/components/currencies/currencyDetail/CurrenciesDetail.tsx
+++ src/components/currencies/currencyDetail/CurrenciesDetail.tsx
@@ -1,21 +1,12 @@
 import { Coin } from "../../../types/types";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../../store/store";
-import { useEffect } from "react";
-import { fetchChartData } from "../../../store/currencies/currencies-actions";
 import classes from "./CurrenciesDetail.module.scss";
 import { CurrencyStats } from "./currencyStats/CurrencyStats";
 import { NextSeo } from "next-seo";
 
 export const CurrenciesDetail = ({ item }: { item: Coin }) => {
-  const dispatch = useDispatch<AppDispatch>();
-
-  useEffect(() => {
-    dispatch(fetchChartData(item.id));
-  }, [dispatch]);
   return (
     <>
-      <NextSeo title={`${item.name} | Crypto Tracker App`} />
+      <NextSeo title={`${item.name} | Crypto Tracker`} />
       <div className={classes.container}>
         <CurrencyStats item={item} />
       </div>
