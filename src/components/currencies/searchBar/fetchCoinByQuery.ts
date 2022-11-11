691ab7c73585a7a9bb5e81a8f61c75ea7035516a
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-11 21:56:55 +0100

    Refactor: remove unused condition & variables

--- src/components/currencies/searchBar/fetchCoinByQuery.ts
+++ src/components/currencies/searchBar/fetchCoinByQuery.ts
@@ -1,14 +1,7 @@
 export const fetchCoinByQuery = async (query: string) => {
-  try {
-    const res = await fetch(
-      `https://api.coingecko.com/api/v3/search/?query=${query}`
-    );
-    if (!res.ok) {
-      throw new Error("Problem with fetch data");
-    }
-    const data = await res.json();
-    return data.coins;
-  } catch (error) {
-    return error;
-  }
+  const res = await fetch(
+    `https://api.coingecko.com/api/v3/search/?query=${query}`
+  );
+  const data = await res.json();
+  return data.coins;
 };
