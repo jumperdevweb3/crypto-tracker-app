c105574992ba507aa39e0d0e07b347edc971208d
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-11 21:56:22 +0100

    Feat: create pagination component

---
+++ src/components/currencies/paginationBar/PaginationBar.tsx
@@ -0,0 +1,27 @@
+import { useRouter } from "next/router";
+import { Pagination } from "@mantine/core";
+import classes from "./PaginationBar.module.scss";
+
+interface Props {
+  isLoading: boolean;
+  disabled: boolean;
+  page: number | string;
+}
+
+export const PaginationBar = ({ isLoading, disabled, page }: Props) => {
+  const router = useRouter();
+
+  const changePage = (event: number) => {
+    router.push(`/?page=${event}`, undefined, { scroll: false });
+  };
+  const PaginationBar = (
+    <Pagination
+      total={100}
+      className={classes.pagination}
+      page={+page}
+      onChange={changePage}
+      disabled={isLoading || disabled}
+    />
+  );
+  return <>{PaginationBar}</>;
+};
