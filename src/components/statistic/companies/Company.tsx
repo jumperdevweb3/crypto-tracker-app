eba133a4d782c753a007ce8ff0d989464c5b38a4
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-11 22:09:48 +0100

    Feat: isolation variables & add validation

--- src/components/statistic/companies/Company.tsx
+++ src/components/statistic/companies/Company.tsx
@@ -2,7 +2,7 @@ import { CompaniesType } from "../../../types/types";
 import classes from "../exchanges/Exchange.module.scss";
 import { currencyValueFormat } from "../../../helpers/numberFromat";
 
-export const Company = ({ ...props }) => {
+export const Company = ({ item }: { item: CompaniesType }) => {
   const {
     name,
     symbol,
@@ -11,35 +11,53 @@ export const Company = ({ ...props }) => {
     total_entry_value_usd,
     total_current_value_usd,
     percentage_of_total_supply,
-  } = props.item as CompaniesType;
+  } = item as CompaniesType;
 
+  const SymbolContent = !!symbol && (
+    <p>
+      Symbol: <span>{symbol}</span>
+    </p>
+  );
+  const CountryContent = !!country && (
+    <p>
+      Country: <span>{country}</span>
+    </p>
+  );
+  const TotalHoldingsContent = !!total_holdings && (
+    <p>
+      Total holdings: <span>{total_holdings} BTC</span>
+    </p>
+  );
+  const SupplyPercentContent = !!percentage_of_total_supply && (
+    <p className={classes.volume}>
+      Percentage of total supply:{" "}
+      <span>{percentage_of_total_supply.toFixed(4)} BTC</span>
+    </p>
+  );
+  const EntryValueContent = !!total_entry_value_usd && (
+    <p>
+      Total entry value USD:{" "}
+      <span>{currencyValueFormat.format(total_entry_value_usd)}</span>
+    </p>
+  );
+  const CurrentValueContent = !!total_current_value_usd && (
+    <p>
+      Total Current value USD:{" "}
+      <span>{currencyValueFormat.format(total_current_value_usd)}</span>
+    </p>
+  );
   return (
     <div className={classes.container}>
       <div className={classes["name-box"]}>
         <p>{name} </p>
       </div>
       <div className={classes["data-box"]}>
-        <p>
-          Symbol: <span>{symbol}</span>
-        </p>
-        <p>
-          Country: <span>{country}</span>
-        </p>
-        <p>
-          Total holdings: <span>{total_holdings} BTC</span>
-        </p>
-        <p className={classes.volume}>
-          Percentage of total supply:{" "}
-          <span>{percentage_of_total_supply.toFixed(4)} BTC</span>
-        </p>
-        <p>
-          Total entry value USD:{" "}
-          <span>{currencyValueFormat.format(total_entry_value_usd)}</span>
-        </p>
-        <p>
-          Total Current value USD:{" "}
-          <span>{currencyValueFormat.format(total_current_value_usd)}</span>
-        </p>
+        {SymbolContent}
+        {CountryContent}
+        {TotalHoldingsContent}
+        {SupplyPercentContent}
+        {EntryValueContent}
+        {CurrentValueContent}
       </div>
     </div>
   );
