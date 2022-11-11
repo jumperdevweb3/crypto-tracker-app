f638b1b30b3547554086971a4a80c8dd0ee0015e
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-11 22:13:29 +0100

    Refactor: remove unused states & functions

--- src/store/ui/ui-slice.ts
+++ src/store/ui/ui-slice.ts
@@ -3,17 +3,9 @@ import { createSlice } from "@reduxjs/toolkit";
 const uiSlice = createSlice({
   name: "ui",
   initialState: {
-    notification: { message: "" },
-    isLoading: false,
     showNav: false,
   },
   reducers: {
-    showNotification(state, action) {
-      state.notification = { message: action.payload.message };
-    },
-    showLoading(state, action) {
-      state.isLoading = action.payload;
-    },
     showNavigation(state, action) {
       state.showNav = action.payload;
     },
