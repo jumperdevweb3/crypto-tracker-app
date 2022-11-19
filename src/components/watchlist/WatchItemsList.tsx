37f39ad263c9f5a21b160338af16408734c1c7ef
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-19 19:26:53 +0100

    Feat: Create watchlist items component

---
+++ src/components/watchlist/WatchItemsList.tsx
@@ -0,0 +1,21 @@
+import { CurrencyItem } from "../../types/types";
+import CoinCard from "../cards/coinCard/CoinCard";
+import { CurrenciesSortMenu } from "../currencies/currenciesList/currenciesSortMenu/CurrenciesSortMenu";
+import classes from "../currencies/currenciesList/CurrenciesList.module.scss";
+
+export const WatchItemsList = ({ items }: { items: CurrencyItem[] }) => {
+  const ItemsList = items.map((item) => {
+    return <CoinCard key={item.id} item={item} />;
+  });
+  return (
+    <>
+      <h2 className="center-item">
+        Your <span>watchlist</span> items.
+      </h2>
+      <div className={classes["market-list"]}>
+        <CurrenciesSortMenu page={"watchlist"} />
+        {ItemsList}
+      </div>
+    </>
+  );
+};
