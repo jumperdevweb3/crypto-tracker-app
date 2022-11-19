97f6c7c48945c7cf9b908f2f6f8f4cbad4ec2683
skoczy01 <filip.skoczylas00@wp.pl>
2022-11-19 19:26:25 +0100

    Fix: watchlist has old currency data

--- src/components/cards/coinCard/WatchlistButton.tsx
+++ src/components/cards/coinCard/WatchlistButton.tsx
@@ -4,20 +4,18 @@ import { AppDispatch, RootState } from "../../../store/store";
 import { AiFillStar } from "react-icons/ai";
 import { watchlistActions } from "../../../store/watchlist/watchlist-slice";
 import { CurrencyItem } from "../../../types/types";
-export const WatchlistButton = ({
-  classes,
-  id,
-  item,
-}: {
+
+interface Props {
   classes: any;
   id: string;
   item: CurrencyItem;
-}) => {
-  const watchList = useSelector(
-    (state: RootState) => state.watchlist.watchItems
-  );
-  const isItemWatch = watchList.find((watchItem: { id: string }) => {
-    return watchItem.id === id;
+}
+export const WatchlistButton = ({ classes, id, item }: Props) => {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const watchList = useSelector((state: RootState) => state.watchlist.watchIds);
+  const isItemWatch = watchList.find((watchItem) => {
+    return watchItem === id;
   });
 
   const isWatchIcon = isItemWatch ? (
@@ -25,9 +23,9 @@ export const WatchlistButton = ({
   ) : (
     <BiStar color="#8d9904" fontSize="1.25rem" />
   );
-  const dispatch = useDispatch<AppDispatch>();
   const addToWatchlistHandler = () => {
-    dispatch(watchlistActions.updateItems(item));
+    dispatch(watchlistActions.updateIds(item.id));
+    dispatch(watchlistActions.updateItems(item.id));
   };
   return (
     <button className={classes.star} onClick={addToWatchlistHandler}>
