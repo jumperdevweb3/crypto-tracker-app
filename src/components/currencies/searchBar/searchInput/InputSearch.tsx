4fd693b6facb63acdf40cef9bec22f2c3c19c382
skoczy01 <skoczy01@wp.pl>
2022-11-24 14:54:52 +0100

    Refactor: change icons

--- src/components/currencies/searchBar/searchInput/InputSearch.tsx
+++ src/components/currencies/searchBar/searchInput/InputSearch.tsx
@@ -1,23 +1,16 @@
 import classes from "./InputSearch.module.scss";
 import { BiSearch } from "react-icons/bi";
-import { ImCross } from "react-icons/im";
 
 export const InputSearch = ({ ...props }) => {
   return (
     <div className={classes["input-box"]}>
+      <BiSearch className={classes.search} fontSize={"1.2rem"} color={"#aaa"} />
       <input
         type="text"
         placeholder={props.placeholder}
         className={classes.input}
         onChange={props.onChange}
         value={props.value}
-      />
-      <BiSearch className={classes.search} />
-      <ImCross
-        className={classes.cross}
-        fontSize="0.7rem"
-        color="#aaa"
-        onClick={props.onClickCross}
       />
     </div>
   );
