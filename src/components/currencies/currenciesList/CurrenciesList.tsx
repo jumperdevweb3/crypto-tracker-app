6232f69dfadd4c81aec47c3669c1ddf512d6641b
Krzaczor <jbielecki18@gmail.com>
2022-11-29 21:53:25 +0100

    fix rerender page on change number page

--- src/components/currencies/currenciesList/CurrenciesList.tsx
+++ src/components/currencies/currenciesList/CurrenciesList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import { CurrenciesSortMenu } from "./currenciesSortMenu/CurrenciesSortMenu";
 import CoinCard from "../../cards/coinCard/CoinCard";
@@ -8,74 +8,84 @@ import { RootState } from "../../../store/store";
 import { PaginationBar } from "../paginationBar/PaginationBar";
 import { useQuery } from "react-query";
 import { getCurrenecies } from "./getCurrencies";
-import { useRouter } from "next/router";
 import { ICurrencyItem } from "../../../types/types";
 import { changeDataVariables } from "./changeDataVariables";
 import { sortCurrencies } from "../../../helpers/sortCurrencies";
 
+const getParams = () => {
+    return new URLSearchParams(window.location.search);
+}
+
 export const CurrenciesList = () => {
-  const [page, setPage] = useState(1);
-  const router = useRouter();
-  const { sortActive } = useSelector((state: RootState) => state.currencies);
+    const [page, setPage] = useState<number>(1);
+    const firstRender = useRef(true);
+    const { sortActive } = useSelector((state: RootState) => state.currencies);
 
-  const { data, isError, isLoading, status, isPreviousData } = useQuery<
-    ICurrencyItem[]
-  >(["currencies", page], () => getCurrenecies(page, 100), {
-    keepPreviousData: true,
-    refetchInterval: 35000,
-  });
+    const { data, isError, isLoading, status, isPreviousData } = useQuery<ICurrencyItem[]>(
+        ["currencies", page],
+        () => getCurrenecies(page, 100), {
+            keepPreviousData: true,
+            refetchInterval: 35000,
+        }
+    );-
 
-  useEffect(() => {
-    if (typeof router.query.page === "string") {
-      setPage(+router.query.page);
+    useEffect(() => {
+        const params = getParams();
+        const pageParam = params.get('page');
+
+        if (pageParam && !Number.isNaN(parseInt(pageParam))) {
+            setPage(+pageParam);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false;
+            return;
+        }
+
+        const params = getParams();
+
+        params.set('page', `${page}`);
+        history.replaceState(null, '', `?${params.toString()}`);
+    }, [page]);
+
+    const changePage = (page: number) => {
+        setPage(page);
     }
-    if (router.asPath === "/") {
-      setPage(1);
-    }
-  }, [router]);
 
-  const LoadingContent = isLoading && !isPreviousData && <LoadingSpinner />;
-
-  const sortItems =
-    status === "success"
-      ? sortCurrencies(
-          data.map((item) => changeDataVariables(item) as ICurrencyItem),
-          sortActive
+    const sortItems = status === "success"
+        ? sortCurrencies(
+            data.map((item) => changeDataVariables(item) as ICurrencyItem),
+            sortActive
         )
-      : [];
+        : [];
 
-  const ItemsRender = sortItems.map((item) => {
-    return <CoinCard key={item.id} item={item} />;
-  });
-  const SortMenu = !isError && !isLoading && (
-    <CurrenciesSortMenu page={"home"} />
-  );
-  const CurrenciesContent = !isLoading && ItemsRender;
-  const NotFoundContent = !sortItems.length && !isLoading && !isError && (
-    <p className="center">Not found items.</p>
-  );
-  const ErrorContent = isError && <p className="center">Fetch data faild.</p>;
-
-  const MarketListContent = data?.length && (
-    <div className={classes["market-list"]}>
-      {SortMenu}
-      {NotFoundContent}
-      {ErrorContent}
-      {CurrenciesContent}
-    </div>
-  );
-  const PaginationContent = !isError && !isLoading && (
-    <PaginationBar
-      isLoading={isLoading}
-      disabled={isPreviousData || !data}
-      page={page}
-    />
-  );
-  return (
-    <>
-      {LoadingContent}
-      {MarketListContent}
-      {PaginationContent}
-    </>
-  );
+    return (
+        <>
+            {isLoading && !isPreviousData && <LoadingSpinner />}
+            {data?.length && (
+                <div className={classes["market-list"]}>
+                {!isError && !isLoading && (
+                    <CurrenciesSortMenu page={"home"} />
+                )}
+                {!sortItems.length && !isLoading && !isError && (
+                    <p className="center">Not found items.</p>
+                )}
+                {isError && <p className="center">Fetch data faild.</p>}
+                {!isLoading && sortItems.map((item) => {
+                    return <CoinCard key={item.id} item={item} />;
+                })}
+                </div>
+            )}
+            {!isError && !isLoading && (
+                <PaginationBar
+                    isLoading={isLoading}
+                    disabled={isPreviousData || !data}
+                    page={page}
+                    changePage={changePage}
+                />
+            )}
+        </>
+    );
 };
