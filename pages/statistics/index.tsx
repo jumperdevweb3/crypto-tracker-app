58155c56bc2f8f8dd51190b8834d2b59273915a8
Filip <82238474+skoczy01@users.noreply.github.com>
2022-11-30 16:01:21 +0100

    Feat: add SSG

--- pages/statistics/index.tsx
+++ pages/statistics/index.tsx
@@ -1,13 +1,46 @@
+import {
+  getCompanies,
+  getExchanges,
+  getNfts,
+} from "@/components/statistic/fetchStatistic";
 import { Statistics } from "@/components/statistic/Statistics";
 
 import Head from "next/head";
-export default function StatisticPage() {
+
+interface IProps {
+  nftsData: [];
+  companiesData: [];
+  exchangesData: [];
+}
+export default function StatisticPage({
+  nftsData,
+  companiesData,
+  exchangesData,
+}: IProps) {
   return (
     <>
       <Head>
         <title>Statistic | Crypto Tracker</title>
       </Head>
-      <Statistics />
+      <Statistics
+        initNfts={!!nftsData.length ? nftsData : null}
+        initCompanies={!!companiesData.length ? companiesData : null}
+        initExchanges={!!exchangesData.length ? exchangesData : null}
+      />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const nftsData: [] = await getNfts();
+  const companiesData: [] = await getCompanies();
+  const exchangesData: [] = await getExchanges();
+
+  return {
+    props: {
+      nftsData,
+      companiesData,
+      exchangesData,
+    },
+  };
 }
