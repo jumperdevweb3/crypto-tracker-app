1725e04026b0139d03373b92404ead9ddb90b346
Filip <82238474+skoczy01@users.noreply.github.com>
2022-11-30 15:18:07 +0100

    Feat: add SSR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,34 @@
 import { CurrenciesList } from "@/components/currencies/currenciesList/CurrenciesList";
+import { getCurrenecies } from "@/components/currencies/currenciesList/getCurrencies";
 import { TrendingStats } from "@/components/currencies/trendingStats/TrendingStats";
+import { GetServerSidePropsContext } from "next";
 
-function HomePage() {
+function HomePage({ items }: { items: [] | null }) {
   return (
     <>
       <TrendingStats />
-      <CurrenciesList />
+      <CurrenciesList initItems={items} />
     </>
   );
 }
 
 export default HomePage;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const isQuery = !!context.query.page ? context.query.page : 1;
+  const data: [] = await getCurrenecies(isQuery);
+  if (!data.length) {
+    return {
+      props: {
+        items: null,
+      },
+    };
+  }
+  if (data.length) {
+    return {
+      props: {
+        items: data,
+      },
+    };
+  }
+}
