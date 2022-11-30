04539d6a2eb689bef411e8b37ee45eb3397f306a
Filip <82238474+skoczy01@users.noreply.github.com>
2022-11-30 15:18:30 +0100

    Feat: Add validation

--- src/components/currencies/currenciesList/getCurrencies.ts
+++ src/components/currencies/currenciesList/getCurrencies.ts
@@ -1,11 +1,12 @@
 export const getCurrenecies = async (
-  page: number | string,
+  page: number | string | string[],
   perPage?: number
 ) => {
   const itemsPerPage = perPage ? perPage : 5;
   const res = await fetch(
     `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${itemsPerPage}&page=${page}&sparkline=false&price_change_percentage=1h%2C24h%2C7d`
   );
+  if (!res.ok) return [];
   const data = res.json();
 
   return data;
