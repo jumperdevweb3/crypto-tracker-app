c7e1279e902228343a5e32bba5d3d40cbf95a54a
Filip <82238474+skoczy01@users.noreply.github.com>
2022-12-09 18:17:40 +0100

    Refactor: change import method

--- src/components/ui/loadingFire/LoadingFire.tsx
+++ src/components/ui/loadingFire/LoadingFire.tsx
@@ -1,10 +1,12 @@
 import styles from "./LoadingFire.module.scss";
 import Image from "next/image";
+import loadingImg from "public/loading.gif";
+
 export const LoadingFire = () => {
   return (
     <div className={styles.box}>
       <Image
-        src={"https://app.1inch.io/assets/images/loading.png"}
+        src={loadingImg}
         width={48}
         height={48}
         alt="Loading fire gif from 1inch"
