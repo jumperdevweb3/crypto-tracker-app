a7d194ee3142fcc7ef27b0341fd597bb141caf7a
Filip <82238474+skoczy01@users.noreply.github.com>
2022-12-09 18:11:02 +0100

    Refactor: Try other file extension

--- src/components/ui/loadingFire/LoadingFire.tsx
+++ src/components/ui/loadingFire/LoadingFire.tsx
@@ -3,7 +3,12 @@ import Image from "next/image";
 export const LoadingFire = () => {
   return (
     <div className={styles.box}>
-      <Image src={"/loading.png"} width={48} height={48} alt="Loading fire" />
+      <Image
+        src={"/loading.gif"}
+        width={48}
+        height={48}
+        alt="Loading fire gif from 1inch"
+      />
     </div>
   );
 };
